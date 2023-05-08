a1f637c6931706e29855ce0f27c3b06cf50807fd
Viswanadh Kothakota <55237330+viswa5427@users.noreply.github.com>
2023-05-08 18:40:53 +0530

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,21 +1,22 @@
 import app from 'firebase/app';
 import 'firebase/firestore'
 
-var firebaseConfig = {
+const firebaseConfig = {
 
-    apiKey: "AIzaSyCxM7F1aU2Uy1PPpUN1lWPCb6xGkZORdug",
-  
-    authDomain: "job-listing-63ab2.firebaseapp.com",
-  
-    projectId: "job-listing-63ab2",
-  
-    storageBucket: "job-listing-63ab2.appspot.com",
-  
-    messagingSenderId: "976604506465",
-  
-    appId: "1:976604506465:web:fd50e3a9be1dcd33effd20"
-  
-  };
+  apiKey: "AIzaSyD0nXMJ6FarwVOJvXwR1wfdJrhxJw3JKjg",
+
+  authDomain: "job-listing2-9d891.firebaseapp.com",
+
+  projectId: "job-listing2-9d891",
+
+  storageBucket: "job-listing2-9d891.appspot.com",
+
+  messagingSenderId: "270559374380",
+
+  appId: "1:270559374380:web:1687224b0217a2ef977589"
+
+};
+
 
 // Initialize firebase
 const firebase = app.initializeApp(firebaseConfig);
